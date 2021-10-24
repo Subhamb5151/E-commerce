58cf3a37935bc61e1fa234488ada27ab4e087f2a
unknown <subham.biswas.800@gmail.com>
2021-10-24 22:32:32 +0530

    first

---
+++ src/library/js/app.js
@@ -0,0 +1,153 @@
+
+feather.replace()
+
+var swiper = new Swiper(".mySwiper3", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+const swiperOne = new Swiper('.testimonial-carousel', {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 8000,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+const swipertwo = new Swiper('.related-carousel', {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 41,
+  autoplay: {
+    delay: 8000,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+$(function(){
+window.onscroll = function() {myFunction()};
+
+var navbar = document.querySelector(".bottom");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+});
+$(function(){
+  var nonLinearSlider = document.getElementById('nonlinear');
+
+noUiSlider.create(nonLinearSlider, {
+    connect: true,
+    behaviour: 'tap',
+    start: [0, 10000],
+   
+    range: {
+        // Starting at 500, step the value by 500,
+        // until 4000 is reached. From there, step by 1000.
+        'min': [0],
+        '10%': [500, 500],
+        '50%': [4000, 1000],
+        'max': [10000]
+    }
+});
+var nodes = [
+  document.getElementById('lower-value'), // 0
+  document.getElementById('upper-value')  // 1
+];
+
+// Display the slider value and how far the handle moved
+// from the left edge of the slider.
+nonLinearSlider.noUiSlider.on('update', function (values, handle, unencoded, isTap, positions) {
+  nodes[handle].innerHTML =   '£'+ values[handle];
+});
+});
+// cart
+function hasClass(ele, cls) {
+    return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+
+function addClass(ele, cls) {
+    if (!hasClass(ele, cls)) ele.className += " " + cls;
+}
+
+function removeClass(ele, cls) {
+    if (hasClass(ele, cls)) {
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        ele.className = ele.className.replace(reg, ' ');
+    }
+}
+
+//Add event from js the keep the marup clean
+function init() {
+    document.getElementById("open-menu").addEventListener("click", toggleMenu);
+    document.getElementById("body-overlay").addEventListener("click", toggleMenu);
+    document.getElementById("close-cart").addEventListener("click", toggleMenu);
+}
+
+//The actual fuction
+function toggleMenu() {
+    var ele = document.getElementsByClassName('cart')[0];
+    if (!hasClass(ele, "menu-open")) {
+        addClass(ele, "menu-open");
+    } else {
+        removeClass(ele, "menu-open");
+    }
+}
+
+//Prevent the function to run before the document is loaded
+document.addEventListener('readystatechange', function() {
+    if (document.readyState === "complete") {
+        init();
+    }
+});
+
+
+
+
---
+++ src/library/js/plugins.js
@@ -0,0 +1,10 @@
+module.exports = {
+    JS_Library: [
+        "./node_modules/jquery/dist/jquery.js",
+        "./node_modules/bootstrap/dist/js/bootstrap.bundle.js",
+        "./node_modules/feather-icons/dist/feather.min.js",
+        "./node_modules/swiper/swiper-bundle.min.js",
+        './node_modules/nouislider/dist/nouislider.js',
+        "./src/library/js/app.js"
+    ]
+};
